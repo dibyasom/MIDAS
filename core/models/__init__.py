495e63da93fa0b5fe0d0c43e12dbdfcbdb4ebc15
Dibyasom <dibya.secret@gmail.com>
2022-04-28 19:39:35 +0530

    Sanity test

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,19 +1,19 @@
 # Importing all core models.
 from .conference import Conference
-from .announcement import Announcement
-from .bulletpoints import BulletPoint
-from .publication_partner import PublicationPartner
-from .technical_partner import TechnicalPartner
-from .schedule_for_early_track import ScheduleForEarlyTrack
-from .schedule_for_regular_track import ScheduleForRegularTrack
-from .venue import Venue
-from .track_for_paper import TrackForPaper
-from .speaker import Speaker
-from .stakeholder import StakeHolder
-from .steering_committee import SteeringCommittee
-from .national_advisory import NationalAdvisoryCommittee
-from .international_advisory import InternationalAdvisoryCommittee
-from .organising_committee import OrganisingCommittee
-from .student_organising_committee import StudentOrganisingCommittee
-from .special_session import SpecialSession
-from .technical_program_committee import TechnicalProgramCommittee
+# from .announcement import Announcement
+# from .bulletpoints import BulletPoint
+# from .publication_partner import PublicationPartner
+# from .technical_partner import TechnicalPartner
+# from .schedule_for_early_track import ScheduleForEarlyTrack
+# from .schedule_for_regular_track import ScheduleForRegularTrack
+# from .venue import Venue
+# from .track_for_paper import TrackForPaper
+# from .speaker import Speaker
+# from .stakeholder import StakeHolder
+# from .steering_committee import SteeringCommittee
+# from .national_advisory import NationalAdvisoryCommittee
+# from .international_advisory import InternationalAdvisoryCommittee
+# from .organising_committee import OrganisingCommittee
+# from .student_organising_committee import StudentOrganisingCommittee
+# from .special_session import SpecialSession
+# from .technical_program_committee import TechnicalProgramCommittee
